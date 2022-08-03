833468abed971fe636ee6a2501574d3395203147
ibnshayed <emranibnshayed@gmail.com>
2022-08-04 05:41:39 +0600

    success api added

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,15 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "../frontend", "build")))
-console.log("🚀 ~ file: server.js ~ line 9", path.join(__dirname, "../frontend", "build"))
+// console.log("🚀 ~ file: server.js ~ line 9", path.join(__dirname, "../frontend", "build"))
 
-// app.get("/", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
-// })
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
+})
+
+app.get("/api/success", (req, res) => {
+	res.status(200).json({message: "Server running successfully!"})
+ })
 
 const port = process.env.PORT || 5000
 
