fe21ffd6c3072e8a59342dc77c2ec4e703ea571c
ibnshayed <emranibnshayed@gmail.com>
2022-08-04 06:38:26 +0600

    predev script added

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, "../frontend", "build")))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../frontend", "build")));
 // console.log("🚀 ~ file: server.js ~ line 9", path.join(__dirname, "../frontend", "build"))
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
-})
+  res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+});
 
 app.get("/api/success", (req, res) => {
-	res.status(200).json({message: "Server running successfully!"})
- })
+  res.status(200).json({ message: "Server running successfully!" });
+});
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log("Server running on port " + port))
+app.listen(port, () =>
+  console.log("Server running on port http://localhost:" + port)
+);
